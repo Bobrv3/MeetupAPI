a2b1fdb10b84dece66577a8e1461bbc97020764c
Anton Bobrov <anton.bobrov.98@gmail.com>
2022-10-28 13:45:11 +0300

    chore: added db migrations

---
+++ src/main/resources/db/migration/V2__add_events.sql
@@ -0,0 +1,17 @@
+INSERT INTO meetups (id, topic, description, organizer, event_date, place)
+VALUES (1, 'Joker', 'International conference for experienced Java developers', 'JUGru group',
+        '2022-11-19 09:30', 'Moscow');
+
+INSERT INTO meetups (id, topic, description, organizer, event_date, place)
+VALUES (2, 'Mobius 2022 Autumn', 'Conference for mobile developers', 'JUGru group',
+        '2022-11-21 10:00', 'Moscow');
+
+INSERT INTO meetups (id, topic, description, organizer, event_date, place)
+VALUES (3, 'DUMP Kazan 2022', 'Tatarstan''s central IT conference', 'IT People Connection',
+        '2022-11-11 11:00', 'Kazan');
+
+INSERT INTO meetups (id, topic, description, organizer, event_date, place)
+VALUES (4, 'Joker', 'International conference for experienced Java developers', 'Skolkovo Innovation Center',
+        '2023-11-25 10:00', 'Moscow');
+
+ALTER SEQUENCE meetup_seq RESTART WITH 5;
